8235c140ab9a202184b6d918529aab9e9dc8a725
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2024-08-15 08:21:14 +0800

    Add files via upload

--- install2.sh
+++ install2.sh
@@ -254,9 +254,10 @@ if command -v systemctl &>/dev/null; then
     systemctl start my_script.service
     echo -e "${GREEN}Service has been added to systemd startup.${PLAIN}"
 else
-   echo '${FLIE_PATH}start.sh' > /etc/local.d/my_script.start
-   chmod +x /etc/local.d/my_script.start
-   rc-update add local default
+   echo "#!/bin/bash" > /etc/rc.local
+   echo "${FLIE_PATH}start.sh &" >> /etc/rc.local
+   echo "exit 0" >> /etc/rc.local
+   chmod +x /etc/rc.local
    echo -e "${GREEN}Script has been added to rc.local for startup.${PLAIN}"
    nohup ${FLIE_PATH}start.sh &
 fi
